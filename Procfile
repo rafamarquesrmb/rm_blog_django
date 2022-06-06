52719b88d5d62c49751867560acb9f03e4d9b34a
rafamarquesrmb <rafa.rmb@gmail.com>
2022-06-06 01:20:37 -0300

    heroku deploy configurations

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rmblog.wsgi --log-file -
--- rmblog/settings.py
+++ rmblog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -187,3 +187,5 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+django_heroku.settings(locals())
