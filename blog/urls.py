36e7a4b541c8b1826470f458f21f74f99f19219e
rafamarquesrmb <rafa.rmb@gmail.com>
2022-06-08 18:10:11 -0300

    Refactoring my blog pages with partials templates and my blog.views functions

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('<slug:slug>/', views.category, name='category_detail'),
     path('<slug:category_slug>/<slug:slug>/', views.detail, name='post_detail'),
 
-
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,15 @@ from .models import Category, Post, Tag
 def blog(request):
     posts = Post.objects.filter(status=Post.ACTIVE)
     posts_paginated = pagination(request, posts)
-    return render(request, 'blog/blog.html', {'posts': posts_paginated})
+    context = {
+        'page_title': 'Blog',
+        'page_metatitle': 'A blog about Programming and technology.',
+        'banner_background_image_slug': 'assets/img/home-bg.jpg',
+        'banner_heading': 'RM Blog',
+        'banner_subheading': 'A blog about Programming and technology.',
+        'posts': posts_paginated,
+    }
+    return render(request, 'blog/posts.html', context)
 
 
 def detail(request, category_slug, slug):
@@ -33,14 +41,30 @@ def category(request, slug):
     category_selected = get_object_or_404(Category, slug=slug)
     posts = category_selected.posts.filter(status=Post.ACTIVE)
     posts_paginated = pagination(request, posts)
-    return render(request, 'blog/category.html', {'category': category_selected, 'posts': posts_paginated})
+    context = {
+        'page_title': f'Tag: {category_selected.title}',
+        'page_metatitle': category_selected.meta_description,
+        'banner_background_image_slug': 'assets/img/home-bg.jpg',
+        'banner_heading': category_selected.title,
+        'banner_subheading': f'Showing all posts of the "{category_selected.title}" tag',
+        'posts': posts_paginated,
+    }
+    return render(request, 'blog/posts.html', context)
 
 
 def tags(request, slug):
-    tag_selected = get_object_or_404(Tag, slug=slug)
+    tag_selected: Tag = get_object_or_404(Tag, slug=slug)
     posts = tag_selected.posts.filter(status=Post.ACTIVE)
     posts_paginated = pagination(request, posts)
-    return render(request, 'blog/tags.html', {'tag': tag_selected, 'posts': posts_paginated})
+    context = {
+        'page_title': f'Tag: {tag_selected.title}',
+        'page_metatitle': tag_selected.meta_description,
+        'banner_background_image_slug': 'assets/img/home-bg.jpg',
+        'banner_heading': tag_selected.title,
+        'banner_subheading': f'Showing all posts of the "{tag_selected.title}" tag',
+        'posts': posts_paginated,
+    }
+    return render(request, 'blog/posts.html', context)
 
 
 def search(request):
@@ -48,16 +72,29 @@ def search(request):
     posts = Post.objects.filter(Q(title__icontains=query) | Q(intro__icontains=query) | Q(body__icontains=query),
                                 status=Post.ACTIVE)
     posts_paginated = pagination(request, posts)
-    return render(request, 'blog/search.html', {'posts': posts_paginated, 'query': query})
+    subheading = f'Showing all search results for: "{query}"'
+    context = {
+        'page_title': 'Search Results',
+        'page_metatitle': 'Meta Description',
+        'banner_background_image_slug': 'assets/img/home-bg.jpg',
+        'banner_heading': 'Search Results',
+        'banner_subheading': subheading,
+        'posts': posts_paginated,
+    }
+    return render(request, 'blog/posts.html', context)
 
 
-#UTIL
+# UTIL
 def pagination(request, posts):
-    page = request.GET.get('page', '1')
-    page_limit = request.GET.get('limit', '10')
+    # SETTINGS:
+    default_page = 1
+    default_limit = 2
+
+    page = request.GET.get('page', f'{default_page}')
+    page_limit = request.GET.get('limit', f'{default_limit}')
 
     if not (page_limit.isdigit() and int(page_limit) > 0):
-        page_limit = '10'
+        page_limit = f'{default_limit}'
     posts_paginator = Paginator(posts, page_limit)
     try:
         return posts_paginator.page(page)
